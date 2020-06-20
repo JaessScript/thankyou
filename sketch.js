d25653eae22f13f3874ef00f71b3d5d309c76fe1
JaessScript <jaess.script@gmail.com>
2020-06-20 11:05:29 +0200

    Fixing responsiveness and adding next link

--- sketch.js
+++ sketch.js
@@ -15,18 +15,15 @@ function setup() {
 	canvas.style('z-index', '-1');
 
 	next = select('#next');
-	next.position(windowWidth / 2, 5);
 	next.style('font-size', '1.5em');
 	setInterval(changeColor, 500);
 
 	p = select('#text');
-	p.position(windowWidth / 6, windowHeight / 5);
 	p.style('color','white');
 	p.style('font-family','courier');
 	p.style('font-size','2em');
 
 	img = select('#img');
-	img.position(windowWidth / 3, windowHeight / 2);
 	img.style('max-width', '100%');
 	img.style('max-height', '100%');
 }
